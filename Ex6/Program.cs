074f19cd6bde7665443bb7ba98cf5f19df3af100
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-10 18:49:54 +0300

    re

--- Ex6/Program.cs
+++ Ex6/Program.cs
@@ -5,7 +5,7 @@
         int picturesInRow = 3;
         int pictures = 52;
         int filledRows = pictures / picturesInRow;
-        int ExtraPictures = pictures % picturesInRow;
+        int extraPictures = pictures % picturesInRow;
         Console.WriteLine("Количество заполненых стен = {0}\nКоличество лишних картинок = {1}", filledRows, ExtraPictures);
     }
 }
