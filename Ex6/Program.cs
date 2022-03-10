0fc09857bd1ce2cf3241317fb37609301c4b56c7
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-10 17:54:49 +0300

    Добавьте файлы проекта.

---
+++ Ex6/Program.cs
@@ -0,0 +1,10 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int picturesInARow = 3;
+        int picturesAmount = 52;
+        Console.WriteLine("Количество заполненых рядов = " + picturesAmount / picturesInARow);
+        Console.WriteLine("Количество лишних картинок = " + picturesAmount % picturesInARow); 
+    }
+}
